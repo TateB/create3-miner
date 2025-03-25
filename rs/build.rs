47eb6d2a0dbc7e6757b9abeaaec655a226f04877
tate <tate@ens.domains>
2025-03-25 17:10:59 -0400

    create2 compile

--- rs/build.rs
+++ rs/build.rs
@@ -3,7 +3,7 @@ use std::path::PathBuf;
 use std::process::Command;
 
 fn main() {
-    println!("cargo:rerun-if-changed=src/shader/Keccak256.cu");
+    println!("cargo:rerun-if-changed=src/shader/CREATE2.cu");
 
     // Only compile CUDA if we're on a system with nvcc
     if Command::new("nvcc").arg("--version").output().is_ok() {
@@ -13,12 +13,12 @@ fn main() {
         // Compile CUDA to PTX
         let status = Command::new("nvcc")
             .args(&[
-                "-ccbin=C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\VC\\Tools\\MSVC\\14.42.34433\\bin\\Hostx64\\x64\\cl.exe",
+                // "-ccbin=C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\VC\\Tools\\MSVC\\14.42.34433\\bin\\Hostx64\\x64\\cl.exe",
                 "--ptx",
                 "-arch=sm_50", // Minimum compute capability
                 "-o",
-                &out_dir.join("Keccak256.ptx").to_str().unwrap(),
-                &cuda_dir.join("Keccak256.cu").to_str().unwrap(),
+                &out_dir.join("CREATE2.ptx").to_str().unwrap(),
+                &cuda_dir.join("CREATE2.cu").to_str().unwrap(),
             ])
             .status()
             .expect("Failed to execute nvcc");
@@ -28,11 +28,30 @@ fn main() {
         }
 
         // Copy PTX file to shader directory
-        std::fs::copy(
-            out_dir.join("Keccak256.ptx"),
-            cuda_dir.join("Keccak256.ptx"),
-        ).expect("Failed to copy PTX file");
+        std::fs::copy(out_dir.join("CREATE3.ptx"), cuda_dir.join("CREATE3.ptx"))
+            .expect("Failed to copy PTX file");
+
+        // Compile CUDA to PTX
+        let status = Command::new("nvcc")
+            .args(&[
+                // "-ccbin=C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\VC\\Tools\\MSVC\\14.42.34433\\bin\\Hostx64\\x64\\cl.exe",
+                "--ptx",
+                "-arch=sm_50", // Minimum compute capability
+                "-o",
+                &out_dir.join("CREATE3.ptx").to_str().unwrap(),
+                &cuda_dir.join("CREATE3.cu").to_str().unwrap(),
+            ])
+            .status()
+            .expect("Failed to execute nvcc");
+
+        if !status.success() {
+            panic!("Failed to compile CUDA shader");
+        }
+
+        // Copy PTX file to shader directory
+        std::fs::copy(out_dir.join("CREATE3.ptx"), cuda_dir.join("CREATE3.ptx"))
+            .expect("Failed to copy PTX file");
     } else {
         println!("cargo:warning=nvcc not found, skipping CUDA compilation");
     }
-} 
+}
